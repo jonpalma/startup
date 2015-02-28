a23c38fa533441e58785730b59e37b6d57b1b1f4
lazaroReyes <lazaro.reyesfierro@gmail.com>
2015-02-28 16:29:42 -0700

    encoding utf-8

--- agregar-proyecto.php
+++ agregar-proyecto.php
@@ -1,8 +1,13 @@
 <?php session_start(); ?>
 <?php 
-    if(isset($_SESSION['user'])) {
-            $id_user = $_SESSION['user'];
-            echo $id_user;
+    
+    if(!isset($_POST['project_name']) || !isset($_POST['project_description'])) {
+        header('Location:create.php');
+    } else {
+            if(isset($_SESSION['user'])) {
+                $id_user = $_SESSION['user'];
+                echo $id_user;
+            }
     }
 ?>
 
