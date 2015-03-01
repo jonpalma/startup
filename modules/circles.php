854848ab98653c74cb200e9dd6e1689f1e880df1
jonpalma <jon.palma@mixen.mx>
2015-02-28 22:15:44 -0700

    Interface preguntas

---
+++ modules/circles.php
@@ -0,0 +1,6 @@
+    <div class="col-md-2">
+        <p><span class="circle-1"></span></p>
+        <p><span class="circle-2"></span></p>
+        <p><span class="circle-2"></span></p>
+        <p><span class="circle-2"></span></p>
+    </div>
---
+++ modules/menu-sup.php
@@ -0,0 +1,7 @@
+   <div class="container-fluid">
+       <div class="row top-menu">
+           <div class="col-md-6">Back</div>
+           <div class="col-md-6">Usuario</div>
+
+       </div>
+   </div>
