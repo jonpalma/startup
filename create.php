3ef7e083cd467fe86cc3f72621e3e2c3a34f9b2e
jonpalma <jon.palma@mixen.mx>
2015-02-28 18:25:40 -0700

    Diseño interface para la vista de proyectos

--- create.php
+++ create.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <meta charset="iso-8859-1">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
